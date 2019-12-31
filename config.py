57af3d9b75048505621557984a7488ae59da7028
winkool <ffirecent@gmail.com>
2019-12-31 06:34:20 +0300

    Готовлюсь подключать БД

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Configuration(object):
     DEBUG = True
-    #SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:789852123@localhost/test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:789852123@localhost/test'
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from app import app
+from app import db
 import view
 
 
