da0b3abb019b6a18596259a0a7f7d0474e69d282
winkool <ffirecent@gmail.com>
2020-01-04 04:11:17 +0300

    Готовлюсь подключать БД

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Configuration(object):
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:789852123@localhost/test'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://user1:741456963@localhost/test'
---
+++ models.py
@@ -0,0 +1,104 @@
+from app import db
+from datetime import datetime
+import re
+
+
+def latinizator(letter):
+    legend = {
+        'а': 'a',
+        'б': 'b',
+        'в': 'v',
+        'г': 'g',
+        'д': 'd',
+        'е': 'e',
+        'ё': 'yo',
+        'ж': 'zh',
+        'з': 'z',
+        'и': 'i',
+        'й': 'y',
+        'к': 'k',
+        'л': 'l',
+        'м': 'm',
+        'н': 'n',
+        'о': 'o',
+        'п': 'p',
+        'р': 'r',
+        'с': 's',
+        'т': 't',
+        'у': 'u',
+        'ф': 'f',
+        'х': 'h',
+        'ц': 'ts',
+        'ч': 'ch',
+        'ш': 'sh',
+        'щ': 'shch',
+        'ъ': 'y',
+        'ы': 'y',
+        'ь': "i",
+        'э': 'e',
+        'ю': 'yu',
+        'я': 'ya',
+
+        'А': 'A',
+        'Б': 'B',
+        'В': 'V',
+        'Г': 'G',
+        'Д': 'D',
+        'Е': 'E',
+        'Ё': 'Yo',
+        'Ж': 'Zh',
+        'З': 'Z',
+        'И': 'I',
+        'Й': 'Y',
+        'К': 'K',
+        'Л': 'L',
+        'М': 'M',
+        'Н': 'N',
+        'О': 'O',
+        'П': 'P',
+        'Р': 'R',
+        'С': 'S',
+        'Т': 'T',
+        'У': 'U',
+        'Ф': 'F',
+        'Х': 'H',
+        'Ц': 'Ts',
+        'Ч': 'Ch',
+        'Ш': 'Sh',
+        'Щ': 'Shch',
+        'Ъ': 'Y',
+        'Ы': 'Y',
+        'Ь': "I",
+        'Э': 'E',
+        'Ю': 'Yu',
+        'Я': 'Ya',
+    }
+
+    for i, j in legend.items():
+        letter = letter.replace(i, j)
+    return letter
+
+
+def slugify(s):
+    pattern = r'[^\w+]'
+    return re.sub(pattern, '-', latinizator(s))
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(140))
+    slug = db.Column(db.String(140), unique=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
+
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return '<Post id: {}, title: {}>'.format(self.id, self.title)
+
