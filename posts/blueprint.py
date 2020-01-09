f43fa3b2a2130e74a4edfa46a21dea270cf823d1
winkool <ffirecent@gmail.com>
2020-01-09 16:54:47 +0300

    добавлена кнопка с ссылкой на /blog/create

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,12 +1,8 @@
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from models import Post, Tag
-from flask import request
 from .forms import PostForm
 from app import db
-from flask import redirect
-from flask import url_for
-import json
+
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
@@ -33,9 +29,10 @@ def create_post():
 def index():
     q = request.args.get('q')
     if q:
-        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+        posts = Post.query.filter(Post.title.contains(q)
+                                  | Post.body.contains(q)).order_by(Post.created.desc()).all()
     else:
-        posts = Post.query.all()
+        posts = Post.query.order_by(Post.created.desc()).all()
 
     return render_template('posts/index.html', posts=posts)
     # reqTemp = {}
